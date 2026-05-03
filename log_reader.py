9a448470042a0cc86463ae4a5675d14914b90563
FoxExe <fox.axon@gmail.com>
2026-05-04 03:49:47 +0500

    Statistics for log_reader

--- log_reader.py
+++ log_reader.py
@@ -1,14 +1,15 @@
 import struct
 import sys
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
-
-LOG_DUMP = "log_67-71.bin"
+LOG_DUMP = "log_72.bin"
 
 
 BLOCK_SIZE = 64 * 1024
 MAGIC = 0xDDCC
+
+MAX_GPS_PAUSE = 10 * 60  # Max pause between records. GPS Tracks separator for statistics
 
 # NOTE: H=uint16, I=uint32, B=uint8, f=float, b=int8 (signed)
 S_HEAD = struct.Struct('<H')
@@ -25,21 +26,34 @@ ENC_TYPES = {
 }
 
 
-def hex_string(mac: bytes, separator: str = ":"):
-	lines = []
+def to_hex_str(mac: bytes, separator: str = ":"):
+	text = []
 	for c in mac:
-		lines.append(f"{c:02X}")
-	return separator.join(lines)
+		text.append(f"{c:02X}")
+	return separator.join(text)
 
 
 if __name__ == "__main__":
 	# NOTE: If not work, try this:
-	# python.exe -X utf8 log_reader.py > log_dump_8.txt
+	# python.exe -X utf8 log_reader.py > log.txt
 
 	if sys.platform == "win32":
 		os.system('chcp 65001 > nul')
 	sys.stdout.reconfigure(encoding='utf-8')
 	sys.stderr.reconfigure(encoding='utf-8')
+
+	if not os.path.exists(LOG_DUMP):
+		print(f"File {LOG_DUMP} not found!")
+		exit(1)
+
+	block_stats = {}
+	last_ts = 0
+	last_pts = 0
+	tracks = []
+
+	gps_cnt = 0
+	track_start_ts = None
+	track_start_pos = None
 
 	with open(LOG_DUMP, "rb") as f:
 		f.seek(0, os.SEEK_END)
@@ -49,8 +63,6 @@ if __name__ == "__main__":
 		if file_size < BLOCK_SIZE or file_size % BLOCK_SIZE != 0:
 			print("Wrong file size!")
 			exit(1)
-
-		stats = {}
 
 		for blk_id in range(file_size // BLOCK_SIZE):
 			blk_start = blk_id * BLOCK_SIZE
@@ -64,15 +76,26 @@ if __name__ == "__main__":
 				print(f"Wrong block header ({header:04X}) at {blk_id * BLOCK_SIZE} (Block #{blk_id})!")
 				exit(2)
 
-			while (pos := f.tell()) < file_size:
+			while (pos := f.tell()) < blk_start + BLOCK_SIZE:
 				ts, lat, lon, alt, acc, bat, cnt = S_GPS.unpack(f.read(S_GPS.size))
 
 				if ts == 0xFFFFFFFF:
 					break
 
+				if track_start_pos is None:
+					track_start_ts = ts
+					track_start_pos = pos
+					last_pts = gps_cnt
+				elif ts - last_ts > MAX_GPS_PAUSE:
+					tracks.append((track_start_pos, blk_id, gps_cnt - last_pts, track_start_ts, last_ts))
+					track_start_pos = pos
+					track_start_ts = ts
+					last_pts = gps_cnt
+
+				last_ts = ts
+				gps_cnt += 1
 				dt = datetime.fromtimestamp(ts)
 
-				gps_cnt += 1
 				print(f"--- Point #{gps_cnt} in block #{blk_id} --")
 				print(f"0x{pos:08X} >>>> [{dt}]  {lat:.5f},{lon:.5f}  ALT: {alt}m  ACC: {acc}m  BAT: {bat}%")
 				for i in range(cnt):
@@ -113,34 +136,23 @@ if __name__ == "__main__":
 
 					# Go back to GPS RSSI records
 					f.seek(prev_pos)
-					print(f"[0x{(blk_start + off):08X}] ch. {ch:<2d}  {hex_string(mac)}  {ENC_TYPES.get(enc, "UNKNOWN"):<8s}  {ssid}")
+					print(f"[0x{(blk_start + off):08X}] ch. {ch:<2d}  {to_hex_str(mac)}  {ENC_TYPES.get(enc, "UNKNOWN"):<8s}  {ssid}")
 
-			stats[blk_id] = (gps_cnt, len(aps))
+			block_stats[blk_id] = (gps_cnt, len(aps))
 
-		print("-" * 64)
-		print("Block  GPS  APs")
-		for i, s in stats.items():
-			print(f"{(i + 1):5d} {s[0]:4d} {s[1]:4d}")
+	if track_start_pos is not None:
+		tracks.append((track_start_pos, blk_id, gps_cnt - last_pts, track_start_ts, last_ts))
 
+	print("-" * 64)
+	print(f"{'Offset':<8} | Block | Track | {'Start Time':<19} | {'End Time':<19} | Duration | Points")
+	for i, s in enumerate(tracks, 1):
+		pos, blk, cnt, ts_start, ts_end = s
+		dts = datetime.fromtimestamp(ts_start)
+		dte = datetime.fromtimestamp(ts_end)
+		dtr = dte - dts
+		print(f"{pos:08X} | {blk:>5} | {i:>5} | {dts} | {dte} | {str(dtr):>8} | {cnt:>6}")
 
-"""
-		ap_info = {}
-		f.seek(-S_AP.size, 2)  # go to end of file
-		while True:
-			pos = f.tell()
-			mac, ch_enc, size = S_AP.unpack(f.read(S_AP.size))
-			if size == 0xFF:
-				break
-			ch = (ch_enc >> 4) & 0x0F
-			enc = ch_enc & 0x0F
-			f.seek(f.tell() - S_AP.size - size)
-			ssid = f.read(size).decode('utf-8').strip('\0').strip()
-			print(f"0x{pos:08X}:", end=" ")
-			for c in mac:
-				print(f"{c:02X}", end=" ")
-			print(f"- {ENC_TYPES.get(enc, "UNKNOWN"):>8s} @ {ch:2d} \"{ssid}\" ")
-
-			ap_info[pos] = (mac, ssid, ch, enc)
-
-			f.seek(pos - size - S_AP.size)
-"""
+	print("-" * 64)
+	print("Block   GPS  APs")
+	for i, s in block_stats.items():
+		print(f"{i:5d}  {s[0]:4d}  {s[1]:4d}")
